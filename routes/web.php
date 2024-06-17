d93bdbaa012e492671d45dd324b2bedc7e86bc84
Charles Gendron <charlesgendron@Charless-MacBook-Pro.local>
2024-06-17 05:26:49 -0400

    reusing blade code

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
@@ -42,12 +31,6 @@ Route::get('/tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    // $data = $request->validated();
-    // $task = new Task;
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -55,11 +38,6 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function ($task, Request $request) {
-    // $data = ;
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -71,18 +49,6 @@ Route::delete('/tasks/{task}', function (Task $task) {
     return redirect()->route('tasks.index')
         ->with('success','Task has been deleted successfully!');
 })->name('tasks.destroy');
-
-// Route::get('/hello', function () {
-//     return 'Hello';
-// })->name('hello');
-
-// Route::get('/hallo', function () {
-//     return redirect()->route('hello');
-// });
-
-// Route::get('/greet/{name}', function ($name) {
-//     return 'Hello ' . $name . '!';
-// });
 
 Route::fallback(function () {
     return 'Still got somewhere!';
